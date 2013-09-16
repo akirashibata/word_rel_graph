f87b6b550141c9aad4d0d264e2c3d9f4d119596d
Akira Shibata <akira.shibata@shiroyagi.co.jp>
2013-09-16 16:10:30 +0900

    rel_model done

--- graph_io.py
+++ graph_io.py
@@ -7,13 +7,19 @@ class GraphIO(object):
 
   def relationships(self, word):
     """returns { word_self:count, word_related:count, word_related:count ... }"""
+    print 'returning relationships of word', word
+    if word==11489:
+      return {11489:4, 1413:3, 23523:2, 929:3}
+    if word==11491:
+      return {11491:5, 89129:4, 9812912:2}
 
   def appearance(self, word):
     """word appearance. add node if not in db or increment count"""
+    print 'appearance of word', word
 
   def co_appearance(self, word_a, word_b):
     """two words appearing in the same document. create edge if not any, increment one otherwise"""
-
+    print 'co appearance of word', word_a, word_b
 
 if __name__=="__main__":
   db=GraphIO()
--- rel_model.py
+++ rel_model.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#----------------------------------------------------------------------------
+#
+#                ,,~~--___---,         Written by
+#               /             .~,
+#           /  _,~             )         Akira Shibata
+#          (_-(~)   ~, ),,,(  /'           for Shiroyagi Corp.
+#          Z6  .~`' ||     \ |　　　　
+#          /_,/     ||      ||               akira.shibata@shiroyagi.co.jp
+#    ~~~~~~~~~~~~~~~W`~~~~~~W`~~~~~~~~~
+#                                               @all rights reserverd
+#----------------------------------------------------------------------------
+
 
 import json
 import datetime
@@ -7,11 +22,71 @@ from shiroyagi_server.DbIo.sql_commands import sql_select
 from shiroyagi_alg.BOW.bow import BOW 
 from shiroyagi_alg.BOW.converter import *
 
-from graph_io import relationships, appearance, co_appearance
+from graph_io import GraphIO
 
 class RelModel(object):
-  def __init__(self):
-    pass
+  def __init__(self, feature_db, words_db, graph_db):
+    self.feature_db=feature_db
+    self.words_db=words_db
+    self.graph_db=graph_db
+    
+  def build(self):
+    for feature in self.feature_db.one_feature(): 
+      keys_processed=[]
+      for key, val in feature.items():
+        self.graph_db.appearance(key)
+        for kay2 in keys_processed:
+          self.graph_db.co_appearance(key, key2)       
+
+  def get(self, word_id=None, word=None):
+    if not word_id and not word: return None
+    elif not word_id and word: 
+      word_ids=self.words_db.one_id(word)
+    else:
+      word_ids=list(word_id)
+    rels_norm={}
+    for word_id in word_ids:
+      rels=self.graph_db.relationships(word_id)
+      try: norm=rels.pop(word_id)
+      except: 
+        print 'word not found in rels', word_id, rels
+        raise
+      for wid, val in rels.items():
+        rels_norm[self.words_db.one_word(wid)]=float(val)/norm
+    return rels_norm
+
+class WordsDB(object):
+  def __init__(self, down_to):
+    self.n=down_to
+    self.con, self.cursor=get_con_cursor()
+
+  def _all_words(self):
+    l= sql_select(self.cursor, 'DICT_IDF', ['word_id', 'word'],
+     conditions='word_id < %s',
+     cond_data=(self.n), orderby='word_id', reverse=False, verbose=True)
+    self.cursor.close()
+    self.con.close()
+    return l
+
+  def one_word(self, word_id):
+    l= sql_select(self.cursor, 'DICT_IDF', [ 'word'],
+     conditions='word_id = %s',
+     cond_data=((word_id,)), verbose=True)
+    if len(l)>0: return l[0]
+    return None
+
+  def one_id(self, word):
+    l= sql_select(self.cursor, 'DICT_IDF', [ 'word_id'],
+     conditions='word = %s',
+     cond_data=((word,)), verbose=True)
+    if len(l)>0: return map(lambda x: x[0], l)
+    return None
+
+  def dump_to_file(self, filename):
+    with open (filename, 'w') as f:
+      for word_id, word in self._all_words():
+        print word_id, word.encode('utf-8')
+        f.write(json.dumps({word_id:word})+'\n')
 
 class FeatureDB(object):
   def __init__(self, date_from, date_to, alg_ver='BOW_TF'):
@@ -44,6 +119,21 @@ class FeatureDB(object):
         f.write(json.dumps(data)+'\n')
  
 if __name__=='__main__':
-  filename='news.json'
-  db=FeatureDB(datetime.datetime(2013, 9, 16, 0, 0), datetime.datetime.now(), alg_ver='BOW_TF')
-  for i in db.one_feature(): print i
+  start=datetime.datetime.now()
+  interval=datetime.timedelta(days=1)
+  for i in range(1):
+    time_from=start-interval
+    print 'building model using data from', time_from, 'to', start
+    fdb=FeatureDB(time_from, start, alg_ver='BOW_TF')
+    start=time_from
+
+    wdb=WordsDB(10)
+    print wdb.one_word(10)
+    word='ペン'
+    print wdb.one_id(word)
+
+    gdb=GraphIO()
+
+    model=RelModel(fdb, wdb, gdb)
+    #model.build()
+    print model.get(word='ペン')
